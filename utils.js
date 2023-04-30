5f93c5827cce63d589eb383426a7fff653e45f21
Yunhao-Luo <yhluo101@gmail.com>
2023-04-29 23:16:11 -0700

    create tab initial commit

--- utils.js
+++ utils.js
@@ -736,6 +736,28 @@ function onClickDownloadBtn() {
 		);
 	}
 }
+
+function openTab(evt, tabName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+  
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
+
 const link = document.createElement( 'a' );
 link.style.display = 'none';
 document.body.appendChild( link );
@@ -761,3 +783,4 @@ window.clickConnectBtn = onClickConnectBtn;
 window.clickMinusBtn = onClickMinusBtn;
 window.clickPlusBtn = onClickPlusBtn;
 window.playPause = playPause;
+window.openTab = openTab;
